f06dce8a29cfe6fec751bdef26e7dbb1ae52abee
G-STATION\gilbe <gilbert99009@hotmail.com>
2024-08-15 17:41:32 +0800

    ngrok demo build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "91f59771c117d8621217825550b3f994",
+"flutter_bootstrap.js": "530f95223c917b39d61ef40f45aa4132",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "89bd3fde159b3f4e014d3ba7975537b5",
 "/": "89bd3fde159b3f4e014d3ba7975537b5",
-"main.dart.js": "9871aad50fb2f4cd131131a031afe746",
+"main.dart.js": "6f90960a8b814b6532189da48747585b",
 "manifest.json": "a67a2daa952001f4a5cf3e0c9e1df380",
 "version.json": "7ecec07897e222f4ff712d92a24b51b9"};
 // The application shell files that are downloaded before a service worker can
