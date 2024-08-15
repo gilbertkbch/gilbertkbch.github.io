ec62def03159a77af3a045d1d0fababf91d8c214
G-STATION\gilbe <gilbert99009@hotmail.com>
2024-08-15 20:52:24 +0800

    dev build v0.1.7-ngrok

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3954706558"
+    serviceWorkerVersion: "3750740033"
   }
 });
